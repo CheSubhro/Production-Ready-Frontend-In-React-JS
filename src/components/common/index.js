cbd91904ee76fa4ce4b0ae67393d9c29c3ae6d40
Subhro <92868017+CheSubhro@users.noreply.github.com>
2026-06-04 12:20:29 +0530

    Update index.js

--- src/components/common/index.js
+++ src/components/common/index.js
@@ -1,12 +1,13 @@
 
-export { default as Button } from './Button';
-export { default as Input } from './Input';
-export { default as Spinner } from './Spinner';
-export { default as Modal } from './Modal';
-export { default as Badge } from './Badge';
-export { default as Tooltip } from './Tooltip';
-export { default as ErrorBoundary } from './ErrorBoundary';
-export { default as Card } from './Card';
-export { default as EmptyState } from './EmptyState';
-export { default as ConfirmModal } from './ConfirmModal';
-export { default as Pagination } from './Pagination';
+export { default as Button } from './Button/Button';
+export { default as Input } from './Input/Input';
+export { default as Spinner } from './Spinner/Spinner';
+export { default as Modal } from './Modal/Modal';
+export { default as Badge } from './Badge/Badge';
+export { default as Tooltip } from './Tooltip/Tooltip';
+export { default as ErrorBoundary } from './ErrorBoundary/ErrorBoundary';
+export { default as Card } from './Card/Card';
+export { default as EmptyState } from './EmptyState/EmptyState';
+export { default as ConfirmModal } from './ConfirmModal/ConfirmModal';
+export { default as Pagination } from './Pagination/Pagination';
+export { default as CustomSelect } from './Select/CustomSelect';
